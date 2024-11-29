9d8e8461ac4fa5a0c81569885cd29864dc8186ee
suhas991 <suhas4497@gmail.com>
2024-11-29 14:39:00 +0530

    integrated api

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # integrate the Gemini AI
+from constants import GOOGLE_API_KEY
+import getpass
+import os
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro",google_api_key=GOOGLE_API_KEY)
+
+
+
+st.title('Langchain Demo with Gemini AI')
+input_text = st.text_input("Search the topic u want here")
+output = llm.invoke(input_text)
+st.write(output.content)
+
